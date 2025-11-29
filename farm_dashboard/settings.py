b677a08e5a02a19cc9dbb72b0e5a10a126a0ca47
priyadarshinibaruah <baruahpriyadarshini.2025@yahoo.com>
2025-11-29 17:13:40 +0530

    New Setting Changes

--- farm_dashboard/settings.py
+++ farm_dashboard/settings.py
@@ -121,6 +121,8 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_DIRS = [BASE_DIR / "dashboard" / "static"]
 STATICFILES_DIRS = [BASE_DIR / "farm_dashboard" / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
